3aa5f873bd36b371d3bf98cc497ff7ee9bce0957
Gigamo <gigamo@gmail.com>
2010-01-17 20:20:49 +0100

    Set temporary window title

--- lib/shiken/widgets/main.rb
+++ lib/shiken/widgets/main.rb
@@ -22,12 +22,13 @@ module Shiken
 
       def main_window
         @window = Qt::Widget.new
-        @window.setFixedSize 125, 75
+        @window.set_fixed_size 125, 75
+        @window.set_window_title self.class.to_s
       end
 
       def quit_button
         b = Qt::PushButton.new('Quit', @window)
-        b.setGeometry 25, 25, 75, 25
+        b.set_geometry 25, 25, 75, 25
         Qt::Object.connect b, SIGNAL('clicked()'), options.app, SLOT('quit()')
       end
 
