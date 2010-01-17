d2209b4a51bd0bb4a6c29cff1989dbfaf3c941f3
Gigamo <gigamo@gmail.com>
2010-01-17 20:31:04 +0100

    Show some more on screen

--- lib/shiken/widgets/main.rb
+++ lib/shiken/widgets/main.rb
@@ -13,23 +13,33 @@ module Shiken
       def initialize(options)
         @options = options
 
-        main_window
-        quit_button
+        init_main_window
+        connect_events
         show
       end
 
       private
 
-      def main_window
+      def init_main_window
         @window = Qt::Widget.new
         @window.set_fixed_size 125, 75
         @window.set_window_title self.class.to_s
+        @window.layout = Qt::VBoxLayout.new do |layout| 
+          layout.add_widget title_label
+          layout.add_widget quit_button
+        end
+      end
+
+      def title_label
+        @title_label ||= Qt::Label.new self.class.to_s
       end
 
       def quit_button
-        b = Qt::PushButton.new('Quit', @window)
-        b.set_geometry 25, 25, 75, 25
-        Qt::Object.connect b, SIGNAL('clicked()'), options.app, SLOT('quit()')
+        @quit_button ||= Qt::PushButton.new 'Quit', @window
+      end
+
+      def connect_events
+        Qt::Object.connect quit_button, SIGNAL('clicked()'), options.app, SLOT('quit()')
       end
 
       def show
