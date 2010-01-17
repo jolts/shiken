411d1350974ebf3608f2a9dc9075310a04305d7c
Gigamo <gigamo@gmail.com>
2010-01-17 19:34:05 +0100

    Code formatting

--- lib/shiken/application.rb
+++ lib/shiken/application.rb
@@ -3,11 +3,14 @@ module Shiken
     def self.start
       app = Qt::Application.new(ARGV)
 
-      main_geo = {:height => 75, :width  => 125}
-      quit_geo = {:x => 25, :y => 25, :width => 75, :height => 25}
+      main_geo = {:width => 125, :height => 75}
+      quit_geo = {:width => 75,  :height => 25,
+                  :x     => 25,  :y      => 25}
 
-      Widgets::Windows::Main.new(:geometry => main_geo) do |window|
-        Widgets::Buttons::Quit.new(:app => app, :window => window, :geometry => quit_geo)
+      Widgets::Windows::Main.new   :geometry => main_geo do |window|
+        Widgets::Buttons::Quit.new :app => app,
+                                   :window => window,
+                                   :geometry => quit_geo
       end
 
       Shiken.logger.debug "executing #{app}"
