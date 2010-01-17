5520e15bb354d70570bccd9c22aa8ccfd228d3e5
Gigamo <gigamo@gmail.com>
2010-01-17 19:29:20 +0100

    Prepare

--- lib/shiken/application.rb
+++ lib/shiken/application.rb
@@ -3,9 +3,11 @@ module Shiken
     def self.start
       app = Qt::Application.new(ARGV)
 
-      Widgets::Windows::Main.new(:geometry => {:height => 75, :width  => 125}) do |window|
-        Widgets::Buttons::Quit.new(:app => app, :window => window, :geometry =>
-                                   {:x => 25, :y => 25, :width => 75, :height => 25})
+      main_geo = {:height => 75, :width  => 125}
+      quit_geo = {:x => 25, :y => 25, :width => 75, :height => 25}
+
+      Widgets::Windows::Main.new(:geometry => main_geo) do |window|
+        Widgets::Buttons::Quit.new(:app => app, :window => window, :geometry => quit_geo)
       end
 
       Shiken.logger.debug "executing #{app}"
--- lib/shiken/widgets.rb
+++ lib/shiken/widgets.rb
@@ -1,4 +1,13 @@
 module Shiken
   module Widgets
+=begin
+    def widgets
+      @widgets ||= []
+    end
+
+    def widget(box)
+      widgets << box
+    end
+=end
   end
 end
